6cbe1615f403a9368a5f5ad52cb6aafb912bcaae
Hensley <46694778+Pharmyy@users.noreply.github.com>
2021-02-19 09:15:39 -0500

    Update Feeder.java
    Changed BaseMotorController to TalonFX

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -1,15 +1,18 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoneix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import frc.robot.utils.RobotState;
 import frc.robot.utils.RobotState.PCState;
 
 public class Feeder {
-    public BaseMotorController feeder;
+    public TalonFX feeder;
 
-    public Feeder (BaseMotorController feeder){
+    public Feeder (TalonFX feeder){
         this.feeder = feeder;
     }
 

c8445d5cb17ac282dd8c20a23f5c374cbb417d23
Hensley <46694778+Pharmyy@users.noreply.github.com>
2021-03-01 21:33:17 -0500

    Update Feeder.java
    Spelling 
    import com.ctre.phoneix.motorcontrol.DemandType;
    import com.ctre.phoenix.motorcontrol.DemandType;

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
-import com.ctre.phoneix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import frc.robot.utils.RobotState;
